8604c5465f645c9cd093a2240b68667f13fee49c
Abu Rayhan <ihaveawonderfull@gmail.com>
2026-01-17 14:44:16 +0600

    url change client

--- index.js
+++ index.js
@@ -343,7 +343,8 @@ async function run() {
         }
 
         res.json({
-          gatewayURL: response.GatewayPageURL, 
+          gatewayURL: response.GatewayPageURL,  
+          
           tran_id,
         });
       } catch (err) {
