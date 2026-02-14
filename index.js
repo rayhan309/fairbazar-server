b452b7717834c8c5fb0d1e7ddf5ab0ad09c94c87
mahadi5272 <mahadihasan5272@gmail.com>
2026-02-14 12:36:26 +0600

    added search

--- index.js
+++ index.js
@@ -151,8 +151,6 @@ async function run() {
       }
     });
 
-
-
     app.post("/user", async (req, res) => {
       try {
         const newUser = req.body;
@@ -203,12 +201,15 @@ async function run() {
 
     app.get("/kids", async (req, res) => {
       try {
-        const { category, limit = 0, page = 0 } = req.query;
+        const { category, limit = 0, page = 0 ,search = "" } = req.query;
         let skip = 0;
         if (page > 1) {
           skip = Number(limit) * Number(page - 1);
         }
         const query = {};
+        if (search) {
+          query.title = { $regex: search, $options: "i" }; // "i" মানে case-insensitive
+        }
 
         if (category) {
           query.category = category;
